b09bbe37f38666bb7762b3707814afffa2232cd1
Mouez98 <mouez.matoui@gmail.com>
2022-02-05 14:45:11 +0100

    using an action creator Thunk

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { uiActions } from "./store/cart";
+import { sendCartData } from "./store/cart";
 
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
@@ -14,44 +14,9 @@ function App() {
   const notification = useSelector((state) => state.ui.notification);
 
   useEffect(() => {
-    const sendCartData = async () => {
-      dispatch(
-        uiActions.showNotification({
-          status: "pending",
-          title: "Sending...",
-          message: "Sending cart data!",
-        })
-      );
-      const response = await fetch(
-        "https://food-order-2a829-default-rtdb.europe-west1.firebasedatabase.app/cart",
-        {
-          method: "PUT",
-          body: JSON.stringify(cart),
-        }
-      );
-      dispatch(
-        uiActions.showNotification({
-          title: "success",
-          status: "success",
-          message: "Sent cart data successfully",
-        })
-      );
-      if (!response.ok) {
-       throw new Error('Something weny wrong!')
-      }
-    };
-
     if (cart.totalQuantity !== 0) {
-      sendCartData().catch(error => {
-        dispatch(
-          uiActions.showNotification({
-            title: "Error",
-            status: "error",
-            message: "Sending cart data failed!",
-          })
-        );
-      });
-    }
+      dispatch(sendCartData(cart))
+    }  
   }, [cart, dispatch]);
 
   return (
--- src/store/cart.js
+++ src/store/cart.js
@@ -58,6 +58,50 @@ const uiSlice = createSlice({
   },
 });
 
+export const sendCartData = (cart) => {
+  return async (dispatch) => {
+    dispatch(
+      uiActions.showNotification({
+        status: "pending",
+        title: "Sending...",
+        message: "Sending cart data!",
+      })
+    );
+    const sendRequest = async () => {
+      const response = await fetch(
+        "https://food-order-2a829-default-rtdb.europe-west1.firebasedatabase.app/cart.json",
+        {
+          method: "PUT",
+          body: JSON.stringify(cart),
+        }
+      );
+    
+
+      if (!response.ok) {
+        throw new Error("Something weny wrong!");
+      }
+    };
+    try {
+      await sendRequest();
+      dispatch(
+        uiActions.showNotification({
+          title: "success",
+          status: "success",
+          message: "Sent cart data successfully",
+        })
+      );
+    } catch (error) {
+        dispatch(
+          uiActions.showNotification({
+            title: "Error",
+            status: "error",
+            message: "Sending cart data failed!",
+          })
+        );
+    }
+  };
+};
+
 export const uiActions = uiSlice.actions;
 export const cartActions = cartSlice.actions;
 
